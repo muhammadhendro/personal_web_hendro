577ea1f732de22ceb593ae5abb23ff5c78e5271e
muhammadhendro <muhammadhendro88@gmail.com>
2021-12-31 11:10:04 +0700

    tes desploy

--- connection/db.js
+++ connection/db.js
@@ -9,7 +9,7 @@ const dbPool  = new Pool({
     connectionString : 'postgres://xgecjhiphxoxsg:345193fbbe90dee28124a6de54766e1fa8fa32bb69b202cc05bf2d7ddda4e257@ec2-34-230-133-163.compute-1.amazonaws.com:5432/dkjkj2tstd35g',
     ssl: {
         rejectUnauthorized: false,
-    }
+    },
 })
 
 module.exports = dbPool
